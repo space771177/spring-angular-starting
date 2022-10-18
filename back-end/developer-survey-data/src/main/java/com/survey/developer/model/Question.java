b90184eb5d509706132245781f81bd95ce0130ed
space771177 <nicolas771177@gmail.com>
2022-10-18 14:33:21 +0200

    Change JPA structure of Question Entity

--- back-end/developer-survey-data/src/main/java/com/survey/developer/model/Question.java
+++ back-end/developer-survey-data/src/main/java/com/survey/developer/model/Question.java
@@ -3,6 +3,9 @@ package com.survey.developer.model;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 @Getter
@@ -13,31 +16,44 @@ import java.util.Set;
 @Table(name = "questions")
 public class Question extends BaseEntity{
 
+    @Column(name = "subject")
     private String subject;
 
     @Enumerated(EnumType.STRING)
+    @Column(name = "difficulty")
     private Difficulty difficulty;
-
-    private boolean isMultipleChoices;
 
     @ManyToMany
     @JoinTable(name = "questions_categories_join_table", joinColumns = @JoinColumn(name = "question_id"),
             inverseJoinColumns = @JoinColumn(name = "category_id"))
-    private Set<QuestionCategory> categories;
+    private Set<QuestionCategory> categories = new HashSet<>();
 
-    @ManyToMany
-    @JoinTable(name = "questions_responses_join_table", joinColumns = @JoinColumn(name = "question_id"),
-            inverseJoinColumns = @JoinColumn(name = "response_id"))
-    private Set<QuestionResponse> responses;
+
+    @ElementCollection
+    @CollectionTable(name = "propositions_validation_mapping",
+            joinColumns = {@JoinColumn(name = "question_id", referencedColumnName = "id")})
+    @MapKeyColumn(name = "proposition")
+    @Column(name= "proposition_validate")
+    private Map<String, Boolean> propositions = new HashMap<>();
+
 
     @Builder
-    public Question(Long id, String subject, Difficulty difficulty, boolean isMultipleChoices,
-                    Set<QuestionCategory> categories, Set<QuestionResponse> responses) {
+    public Question(Long id, String subject, Difficulty difficulty, Set<QuestionCategory> categories, Map<String, Boolean> propositions) {
         super(id);
         this.subject = subject;
         this.difficulty = difficulty;
-        this.isMultipleChoices = isMultipleChoices;
         this.categories = categories;
-        this.responses = responses;
+        this.propositions = propositions;
+    }
+
+
+    public void addCategory(QuestionCategory category){
+        this.categories.add(category);
+        category.getQuestions().add(this);
+    }
+
+    public void removeCategory(QuestionCategory category){
+        this.categories.remove(category);
+        category.getQuestions().remove(this);
     }
 }
--- back-end/developer-survey-data/src/main/java/com/survey/developer/model/QuestionCategory.java
+++ back-end/developer-survey-data/src/main/java/com/survey/developer/model/QuestionCategory.java
@@ -3,7 +3,9 @@ package com.survey.developer.model;
 import lombok.*;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -14,6 +16,9 @@ import javax.persistence.Table;
 public class QuestionCategory extends BaseEntity{
 
     private String name;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Question> questions;
 
     @Builder
     public QuestionCategory(Long id, String name) {
